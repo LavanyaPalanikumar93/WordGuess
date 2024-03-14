ce497286ac5ec62a59f728e559f7b82cf6d5c4b0
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-03-14 14:50:18 -0400

    committed

--- src/main/java/com/github/zipcodewilmington/sample/WordGuess.java
+++ src/main/java/com/github/zipcodewilmington/sample/WordGuess.java
@@ -1,68 +1,67 @@
-        package com.github.zipcodewilmington.sample;
-        import java.util.Random;
-        import java.util.Scanner;
-        public class WordGuess {
+package com.github.zipcodewilmington.sample;
+import java.util.Scanner;
+import java.util.Random;
+public class WordGuess {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-                public static void main(String[]args) {
-                    Scanner scanner = new Scanner(System.in) ;
-                    System.out.println("Welcome to WordGuess Game:");
-                    System.out.println("=======================");
-                    System.out.println("Current Guess:");
-                    String a = scanner.next();
-                    System.out.println("You have 3 Guesses");
-                    String[] words ={"Cat","Dog","Bird"};
-                    Random random = new Random();
-                    String secret_Word = words[random.nextInt(words.length)];
-                    int numberOftries=words.length;
+        String[] words = {"cat", "pet", "bird", "dog"};
 
-                    boolean[] guessedLetter=new boolean[(secret_Word.length())];
-                    int Max_Attempts=3;
-                    int attempts =0;
-                    {
-                        while (numberOftries < Max_Attempts) {
-                            for (int i = 0; i < secret_Word.length(); i++) {
-                                if (guessedLetter[i]) {
-                                    System.out.println(secret_Word.charAt(i) + "");
-                                } else {
-                                    System.out.println("_");
-                                }
-                            }
-                            System.out.println("Guess a letter:");
-                            String guess = scanner.nextLine().toLowerCase();
-                            boolean correctGuess = false;
-                            for (int i = 0; i < secret_Word.length(); i++) {
-                                if (secret_Word.charAt(i) == guess.charAt(0)) {
-                                    guessedLetter[i] = true;
-                                    correctGuess = true;
-                                }
-                            }
-                            attempts++;
-                            boolean wordGuessed = true;
-                            for (boolean guessed : guessedLetter) {
-                                if (!guessed) {
-                                    wordGuessed = false;
-                                    break;
-                                }
-                            }
-                            if (wordGuessed) {
-                                System.out.println("Congratulations! You guessed the word" + secret_Word);
-                                break;
-                            }
-                            if (!correctGuess) {
-                                System.out.println("IncorrectGuess:" + (Max_Attempts));
-                                {
-                                    System.out.println("Sorry you run out of Attempts");
-                                }
-                            }
-                            scanner.close();
-                        }
-                    }
+        String secretWord = words[random.nextInt(words.length)];
 
-                }}
+        boolean[] guessedLetters = new boolean[secretWord.length()];
 
+        final int MAX_ATTEMPTS = 3;
 
+        int attempts = 0;
 
+        System.out.println("Welcome to the Word Guess Game!");
 
+        while (attempts < MAX_ATTEMPTS) {
 
+            for (int i = 0; i < secretWord.length(); i++) {
+                if (guessedLetters[i]) {
+                    System.out.print(secretWord.charAt(i) + " ");
+                } else {
+                    System.out.print("_ ");
+                }
+            }
+            System.out.println();
 
+            System.out.print("Guess a letter: ");
+            String guess = scanner.nextLine().toLowerCase();
 
+            boolean correctGuess = false;
+            for (int i = 0; i < secretWord.length(); i++) {
+                if (secretWord.charAt(i) == guess.charAt(0)) {
+                    guessedLetters[i] = true;
+                    correctGuess = true;
+                }
+            }
+
+            attempts++;
+
+            boolean wordGuessed = true;
+            for (boolean guessed : guessedLetters) {
+                if (!guessed) {
+                    wordGuessed = false;
+                    break;
+                }
+            }
+
+            if (wordGuessed) {
+                System.out.println("Congratulations! You guessed the word '" + secretWord + "' correctly in " + attempts + " attempts.");
+                break;
+            }
+
+            if (!correctGuess) {
+                System.out.println("Incorrect guess. Attempts left: " + (MAX_ATTEMPTS - attempts));
+            }
+        }
+        if (attempts >= MAX_ATTEMPTS) {
+            System.out.println("Sorry, you have run out of attempts. The word was '" + secretWord + "'.");
+        }
+        scanner.close();
+    }
+}
